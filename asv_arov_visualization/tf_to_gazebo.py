ab862f4f9224ef544b4fad8feb0142656ea2a776
Chris <63760001+cwloftus@users.noreply.github.com>
2025-06-23 22:17:33 +0000

    Adding package stuff

---
+++ asv_arov_visualization/tf_to_gazebo.py
@@ -0,0 +1,55 @@
+import rclpy
+from rclpy.node import Node
+import tf2_ros
+import gz.transport
+import gz.msgs
+
+class TFToGazebo(Node):
+    def __init__(self):
+        super().__init__('tf_to_gazebo')
+
+        self.link_name = 'arov_link'
+        self.model_name = 'arov'
+        self.world_name = 'tank_world.sdf'
+        self.parent_frame = 'world'
+
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self)
+
+        self.gz_node = gz.transport.Node()
+        self.publisher = self.gz_node.advertise(
+            f"/world/{self.world_name}/model/{self.model_name}/link/{self.link_name}/pose",
+            "gz.msgs.Pose"
+        )
+
+        self.timer = self.create_timer(0.05, self.timer_callback)
+
+    def timer_callback(self):
+        try:
+            tf = self.tf_buffer.lookup_transform(
+                self.parent_frame,
+                self.link_name,
+                rclpy.time.Time()
+            )
+
+            pose_msg = gz.msgs.Pose()
+            pose_msg.position.x = tf.transform.translation.x
+            pose_msg.position.y = tf.transform.translation.y
+            pose_msg.position.z = tf.transform.translation.z
+            pose_msg.orientation.x = tf.transform.rotation.x
+            pose_msg.orientation.y = tf.transform.rotation.y
+            pose_msg.orientation.z = tf.transform.rotation.z
+            pose_msg.orientation.w = tf.transform.rotation.w
+
+            self.publisher.publish(pose_msg)
+        except Exception as e:
+            self.get_logger().warn(f"TF not available: {e}")
+
+def main():
+    rclpy.init()
+    node = TFToGazebo()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
